280276945e94e8a70c5aa0ed1e211454f6f455bd
jasonm10 <67562733+jasonm10@users.noreply.github.com>
2020-07-19 16:25:07 -0700

    Updates

---
+++ precache-manifest.1043a7bb1abb2deb8b0fb345566c245f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a5f05968f6d3af4b1c654c9b6cc34f",
+    "url": "/portfolioskills/index.html"
+  },
+  {
+    "revision": "31e67b6a5442675dbd00",
+    "url": "/portfolioskills/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "afc00948e27f0e331aa2",
+    "url": "/portfolioskills/static/css/main.5fffb87e.chunk.css"
+  },
+  {
+    "revision": "31e67b6a5442675dbd00",
+    "url": "/portfolioskills/static/js/2.71510972.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolioskills/static/js/2.71510972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afc00948e27f0e331aa2",
+    "url": "/portfolioskills/static/js/main.26e7ce77.chunk.js"
+  },
+  {
+    "revision": "796b68b13872ea2f0321",
+    "url": "/portfolioskills/static/js/runtime-main.c5a8371f.js"
+  },
+  {
+    "revision": "7bd16cc6767e1f949899c520f32cea48",
+    "url": "/portfolioskills/static/media/Capriola-Regular.7bd16cc6.woff"
+  }
+]);
